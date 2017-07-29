3c846a8db5dcaa38b94c59f4f655ddff732284b9
Jun <jlee757@uic.edu>
2017-07-29 15:36:20 +0900

    term spinner in schedule fragment

--- app/src/main/java/com/sodastudio/uictime/ui/ScheduleFragment.java
+++ app/src/main/java/com/sodastudio/uictime/ui/ScheduleFragment.java
@@ -7,11 +7,14 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.sodastudio.uictime.CourseLibrary;
 import com.sodastudio.uictime.R;
 import com.sodastudio.uictime.manager.ScheduleManager;
 import com.sodastudio.uictime.model.DetailCourse;
@@ -26,45 +29,62 @@ public class ScheduleFragment extends Fragment {
 
     public static final String TAG = "ScheduleFragment";
 
+    private Spinner mTermSpinner;
+    private ArrayAdapter mTermAdapter;
+
     private RecyclerView mScheduleListView;
     private CourseAdapter mAdapter;
 
-    private ImageButton mImageButton;
-
+    private ImageButton mConciseViewButton;
     private ScheduleManager mScheduleManager;
 
     private Toast mToast;
+
+    private CourseLibrary mCourseLibrary;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_schedule_view, container, false);
 
+        mTermAdapter = ArrayAdapter.createFromResource(getContext(), R.array.term, android.R.layout.simple_spinner_dropdown_item);
+        mTermSpinner = (Spinner)view.findViewById(R.id.schedule_term_spinner);
+        mTermSpinner.setAdapter(mTermAdapter);
+
         mScheduleListView = (RecyclerView)view.findViewById(R.id.schedule_list_view);
         mScheduleListView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
-        mImageButton = (ImageButton)view.findViewById(R.id.concise_view_button);
-        mImageButton.setActivated(false);
+        mConciseViewButton = (ImageButton)view.findViewById(R.id.concise_view_button);
+        mConciseViewButton.setActivated(false);
 
-        mImageButton.setOnClickListener(new View.OnClickListener(){
+        mConciseViewButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                if(!mImageButton.isActivated()){
+                if(!mConciseViewButton.isActivated()){
                     mScheduleListView.setVisibility(View.VISIBLE);
-                    mImageButton.setActivated(true);
+                    mConciseViewButton.setActivated(true);
                 } else {
                     mScheduleListView.setVisibility(View.INVISIBLE);
-                    mImageButton.setActivated(false);
+                    mConciseViewButton.setActivated(false);
                 }
             }
         });
 
+        mCourseLibrary = new CourseLibrary();
         upDateUI();
 
         return view;
     }
 
     private void upDateUI(){
+        int term_id = CourseListFragment.TERM_ID;   // get a term value from previous fragment
+
+        String selected_term = mTermSpinner.getSelectedItem().toString();
+        int selected_term_id = mCourseLibrary.getTermValue(selected_term);
+
+        // TODO:: at first show list from term_id
+        // TODO:: get course list with specified term.  onItemSelectedListener? to notify adapter
+
         ScheduleManager scheduleManager = ScheduleManager.getInstance(getActivity());
         List<DetailCourse> mCourseList = scheduleManager.getCourses();
 
