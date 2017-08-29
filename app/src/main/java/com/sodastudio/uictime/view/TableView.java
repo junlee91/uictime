b1e12b11c8fc86f2559fbd5eae7a66e16bc0588b
Jun <jlee757@uic.edu>
2017-08-28 20:58:34 -0500

    tableview in progress

--- app/src/main/java/com/sodastudio/uictime/view/TableView.java
+++ app/src/main/java/com/sodastudio/uictime/view/TableView.java
@@ -7,9 +7,12 @@ import android.graphics.Paint;
 import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
+import android.widget.FrameLayout;
 
+import com.sodastudio.uictime.R;
 import com.sodastudio.uictime.manager.TableManager;
 import com.sodastudio.uictime.model.DetailCourse;
 import com.sodastudio.uictime.ui.ScheduleFragment;
@@ -23,25 +26,35 @@ public class TableView extends View {
     private static String TAG = TableView.class.getSimpleName();
 
     private TableManager mTableManager;
+    private Context mContext;
 
     public TableView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
 
+        mContext = context;
         mTableManager = TableManager.getInstance();
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        Log.d(TAG, "onDraw()");
+        Log.d(TAG, "onDraw");
         drawTimeTable(canvas);
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        Log.d(TAG, "onMeasure");
+
+        DisplayMetrics dm = mContext.getResources().getDisplayMetrics();
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+
     }
 
     private void drawTimeTable(Canvas canvas){
+
 
         int width = ScheduleFragment.viewWidth;
         int height = ScheduleFragment.viewHeight;
